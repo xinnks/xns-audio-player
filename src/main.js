e0bdea89551a41c3edd18ca4f030298846cc2733
James Sinkala <xinnks@gmail.com>
2019-06-28 20:39:14 +0300

    fixed some issues, scaffold old code with @vue/cli-3

--- src/main.js
+++ src/main.js
@@ -1,17 +1,13 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-require('bulma/css/bulma.min.css')
+import App from './App.vue'
+require('./assets/style.scss')
 
 Vue.config.productionTip = false
 
 Vue.filter('doubleDigits', function (val) {
-  return val < 10 ? '0' + val : val
+    return val < 10 ? '0' + val : val
 })
-/* eslint-disable no-new */
+
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App),
+}).$mount('#app')
