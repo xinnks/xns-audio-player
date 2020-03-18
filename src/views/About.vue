b5c87f02184d7d9b726cdc342d5e784eb7711ce3
James Sinkala <xinnks@gmail.com>
2020-03-18 16:56:36 +0300

    improvement(views/about): ameliorate about page
    make about page look a bit proper

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,10 @@
 <template>
-	<div class="level">
-		<div class="level-item">
+	<div class="tw-flex tw-flex-column tw-h-full tw-w-full tw-m-3 tw-items-center tw-justify-around tw-mt-1 tw-mx-auto tw-mb-3 tw-p-2">
+		<div class="about-content">
 			<h2>About Page</h2>
+			<div class="illustration" >
+				<img src="/img/pluto-page-under-construction.png" alt="">
+			</div>
 		</div>
 	</div>
 </template>
@@ -13,4 +16,12 @@
 </script>
 
 <style>
+.about-container, .illustration, .about-content{
+	text-align: center;
+	align-content: space-around;
+	padding-top: 20px
+}
+.illustration > img {
+	width: 20rem;
+}
 </style>
