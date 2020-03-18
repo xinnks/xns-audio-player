d989702e22d1c7737b30daa067fc06d7fbd267df
James Sinkala <xinnks@gmail.com>
2020-03-18 17:14:34 +0300

    improvement(main.js): update time filters
    update UI playback time filters

--- src/main.js
+++ src/main.js
@@ -10,9 +10,25 @@ Vue.filter('doubleDigits', function (val) {
   if(isNaN(val)){
     return '00'
   }else{
-    return val < 10 ? '0' + val : val
+    if(val < 60){
+      return val < 10 ? '0 : 0' + val.toFixed() : '0 : ' + val.toFixed()
+    } else {
+      let seconds = (val % 60).toFixed() == 60 ? '00' : (val % 60).toFixed()
+      let minutes = Math.floor(val/60).toFixed()
+      return minutes + ' : ' + (seconds < 10 ? '0' + seconds : seconds)
+    }
   }
 })
+
+Vue.filter('secsToMinutes', function (val) {
+  if(isNaN(val)){
+    return (val.toFixed() % 60) + ' : '+ Math.floor(val/60)
+  }else{
+    return 0
+  }
+})
+
+/* eslint-disable */
 
 new Vue({
   store,
