b9fbc84b5dddc3e4056428aa1c08a7827e09c4c1
James Sinkala <xinnks@gmail.com>
2021-04-10 22:29:11 +0300

    Feat(mixins): Move all media playback functions to a separate mixin

--- src/mixins/PlayerMixin.js
+++ src/mixins/PlayerMixin.js
@@ -1,5 +1,7 @@
 import {mapGetters, mapActions, mapState} from 'vuex'
+import PlaybackMixin from './PlaybackMixin'
 const PlayerMixin = {
+  mixins: [PlaybackMixin],
   data(){
     return {
       progressColor: "#21FB92",
@@ -33,55 +35,6 @@ const PlayerMixin = {
     ...mapGetters(['getAudio', 'getSongs', 'getSongsCount', 'getVolume', 'getCurrentTrackId', 'getCurrentTrackTime', 'getCurrentTrackDuration', 'getPlayerIsLoading', 'getPlayerIsPlaying', 'getPlayerIsPaused', 'getPlayerIsStopped', 'getContinuousPlaybackStatus', 'getCurrentTrackId'])
   },
   methods: {
-    emitCurrentSong(){
-      setTimeout(()=>{
-        this.$emit('current-song', this.getSongs(this.activePlayer.position)[this.getCurrentTrackId(this.activePlayer.position)])
-      }, 200)
-    },
-    playCurrentSong(){
-      this.playTrack({playerPosition: this.activePlayer.position, trackId: this.activePlayer.currentTrackId})
-      setTimeout(() =>{
-        this.audioListening()
-        // emit new track
-        this.emitCurrentSong()
-      }, 200)
-    },
-    playSelectedSong(payload){
-      this.playTrack(payload || {playerPosition: this.activePlayer.position, trackId: this.activePlayer.currentTrackId})
-      setTimeout(() =>{
-        this.audioListening()
-        // emit new track
-        this.emitCurrentSong()
-      }, 200)
-    },
-    seekPlayer(time){
-      this.audioListening(false) // stop listening to audio oject
-      this.seekToTime({playerPosition: this.activePlayer.position, time: time}) // seek to given time
-      setTimeout(() =>{
-        this.audioListening() // resume listening to audio oject
-      }, 10)
-    },
-    pauseSong(){
-      this.pause({playerPosition: this.activePlayer.position})
-    },
-    playNextSong(){
-      this.nextSong({playerPosition: this.activePlayer.position})
-      this.audioListening(false)
-      setTimeout(() =>{
-        this.audioListening()
-        // emit new track
-        this.emitCurrentSong()
-      }, 200)
-    },
-    playPrevSong(){
-      this.audioListening(false)
-      this.prevSong({playerPosition: this.activePlayer.position, playlistPosition: this.activePlayer.position})
-      setTimeout(() =>{
-        this.audioListening()
-        // emit new track
-        this.emitCurrentSong()
-      }, 200)
-    },
     audioListening(listen = true){
       if(listen){
         // start listening
